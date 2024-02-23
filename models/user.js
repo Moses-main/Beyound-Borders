e810c0417fe77e616c174a8386d05dccb39d2024
Moses <Moseschizaram8@gmail.com>
2024-02-23 02:01:38 +0100

    corrected the readme file and the user model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,9 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const userSchema = new Schema({
-//   // Define user schema
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
-// models/User.js
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
